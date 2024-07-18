f43959d26aaf66873d44783d4301cf87e3d40a80
keddib <khalid.eddib@gamiumworld.com>
2024-07-18 09:59:48 +0200

    chore: Update Storybook configuration and TypeScript paths

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,31 +1,29 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-import path from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import type { StorybookConfig } from '@storybook/react-vite'
+import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions'
   ],
   framework: {
-    name: "@storybook/react-vite",
-    options: {},
+    name: '@storybook/react-vite',
+    options: {}
   },
-  viteFinal: async (config) => {
+  viteFinal: async config => {
+    config.plugins?.push(
+      tsconfigPaths({
+        projects: [path.resolve((path.dirname(__dirname), '../tsconfig.json'))]
+      })
+    )
 
-    config.plugins?.push(tsconfigPaths({
-      projects: [
-        path.resolve((path.dirname(__dirname), '../tsconfig.json'))
-      ]
-    }));
+    return config
+  }
+}
 
-    return config;
-  },
-};
-
-
-export default config;
+export default config
