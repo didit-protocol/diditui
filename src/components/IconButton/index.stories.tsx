3744f0f4c0607e8b3e7d2a4a63a34eed1bca3923
Khalid Eddib <khalid.eddib@gamiumworld.com>
2024-07-23 16:49:36 +0200

    feat: Add IconButton component with storybook configuration

---
+++ src/components/IconButton/index.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { IconButton } from '.'
+import { svgOptions } from '../Icon'
+
+const icons = Object.keys(svgOptions)
+
+const meta: Meta<typeof IconButton> = {
+  title: 'Components/IconButton',
+  component: IconButton,
+  argTypes: {
+    icon: {
+      control: 'select',
+      options: icons
+    },
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg']
+    }
+  },
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    icon: 'scan',
+    size: 'sm',
+    label: 'Scan QR Code'
+  }
+}
---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,53 @@
+import { IconType } from '@/types'
+import { cn } from '@/utils'
+import { cva, VariantProps } from 'class-variance-authority'
+import { ComponentProps } from 'react'
+import { Icon } from '../Icon'
+import { Text } from '../Text'
+
+const iconButtonStyles = cva(
+  ['bg-surface-lo', 'rounded-full', 'flex items-center justify-center'],
+  {
+    variants: {
+      size: {
+        sm: 'p-[11px]',
+        md: 'p-4',
+        lg: 'p-5'
+      }
+    },
+    defaultVariants: {
+      size: 'sm'
+    }
+  }
+)
+
+type IconButtonProps = ComponentProps<'button'> &
+  VariantProps<typeof iconButtonStyles> & {
+    icon: IconType
+    label?: string
+  }
+
+function IconButton({ icon, label, size, className, ...props }: IconButtonProps) {
+  const buttonClassNames = cn(
+    [
+      'flex flex-col items-center justify-center gap-[6px]',
+      'opacity-70 hover:opacity-100',
+      'transition-opacity duration-300'
+    ],
+    className
+  )
+  return (
+    <button className={buttonClassNames} {...props}>
+      <div className={cn(iconButtonStyles({ size }))}>
+        <Icon type={icon} size={size} />
+      </div>
+      {label && (
+        <Text className="text-inherit" variant="P3">
+          {label}
+        </Text>
+      )}
+    </button>
+  )
+}
+
+export { IconButton }
